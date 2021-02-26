b8abb3348a311d18e0ae3127c9ee7a76a669adef
Conklin, Justin A <jac1047@sru.edu>
2021-02-25 23:01:56 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using NSec.Cryptography;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "C:\\Users\\Pizza\\source\\repos\\CMakeProject2\\CMakeProject2";
+            Span<byte> filetest = new byte[100000];
+
+            if (File.Exists(path)) {
+                byte[] fileb = File.ReadAllBytes(path);
+                int count = 0;
+                foreach (byte b in fileb)
+                {
+                    filetest[count] = b;
+                    count++;
+                }
+
+
+            }
+            
+
+            AeadAlgorithm x= AeadAlgorithm.Aes256Gcm;
+            
+            Span<int> num = new int[1];
+            Sha256 alg= new Sha256();
+            Key a= Key.Create(alg);
+            Nonce n = new Nonce();
+            if(!filetest.IsEmpty)
+            x.Encrypt(a, n,null, filetest);
+            Console.WriteLine("Program dead everyone clap");
+        }
+    }
+}
