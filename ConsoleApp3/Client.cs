338a516a161e33080d1275a0a26a2645c2d0ef16
rcs1014 <55924232+rcs1014@users.noreply.github.com>
2021-03-07 18:12:43 -0500

    Add files via upload

---
+++ ConsoleApp3/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Connection
+{
+    class Client
+    {
+        static void Connect(string server, string path)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpListener
+                // connected to the same address as specified by the server, port
+                // combination.
+                int port = 13000;
+                TcpClient client = new TcpClient(server, port);
+
+                // Get a client stream for reading and writing.
+                NetworkStream stream = client.GetStream();
+
+                //Convert File from parameter "path" to Byte Array for sending.
+                byte[] fileArray = File.ReadAllBytes(path);
+
+                // Send the message to the connected TcpServer.
+                stream.Write(fileArray, 0, fileArray.Length);
+
+                Console.WriteLine("Sent: File");
+
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp3/Listener.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Connection
+{
+    class Listener
+    {
+        static void Recieve()
+        {
+            // Buffer for reading data
+            byte[] fileArray = null;
+
+            //User input save location. GUI input from Browse...
+            string savePath = "";
+
+            //Get local ip somehow...
+            //string localIP = null;          
+
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                int port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.0.202");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(fileArray, 0, fileArray.Length)) != 0)
+                    {
+                        File.WriteAllBytes(savePath, fileArray);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
