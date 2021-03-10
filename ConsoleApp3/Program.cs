27a768ebb9021cce981a500815d1e7ae3109fff2
Saucy <56363885+SaucySensation@users.noreply.github.com>
2021-03-09 21:48:11 -0500

    Add files via upload

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,8 @@
-﻿using NSec.Cryptography;
+﻿
+
 using System;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ConsoleApp3
@@ -10,31 +12,143 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
 
-            string path = "C:\\Users\\Pizza\\source\\repos\\CMakeProject2\\CMakeProject2";
-            Span<byte> filetest = new byte[100000];
+            Aes aesalg = Aes.Create();
 
-            if (File.Exists(path)) {
-                byte[] fileb = File.ReadAllBytes(path);
-                int count = 0;
-                foreach (byte b in fileb)
+
+            string path = "C:\\Users\\Pizza\\source\\repos\\ConsoleApp3\\ConsoleApp3\\test.txt";
+            byte[] fileb;
+
+
+            fileb = File.ReadAllBytes(path);
+            int count = 0;
+
+            foreach (byte b in fileb) { 
+            Console.WriteLine(fileb[count]);
+                count++;
+            }
+            Console.WriteLine("Program dead everyone clap");
+            byte[] encryptedbyte = EncryptFileToBytes_Aes(fileb, aesalg.Key, aesalg.IV);
+            count = 0;
+            foreach (byte b in encryptedbyte)
+            {
+                Console.WriteLine(encryptedbyte[count]);
+                count++;
+            }
+            using (Stream file = File.OpenWrite(@"C:\\Users\\Pizza\\source\\repos\\ConsoleApp3\\ConsoleApp3\\testencrypt.txt"))
+            {
+                file.Write(encryptedbyte, 0, encryptedbyte.Length);
+            }
+            Console.WriteLine("Program dead everyone clap");
+            byte[] outputbyte = DecryptFileFromBytes_Aes(encryptedbyte, aesalg.Key, aesalg.IV);
+            count = 0;
+            
+            foreach (byte b in outputbyte) {
+                Console.WriteLine(outputbyte[count]);
+                count++;
+            }
+
+            /*for (int i =0;i<10 ;++i)
+            Console.WriteLine(encryptedbyte[i]);*/
+
+            //string output = DecryptStringFromBytes_Aes(encryptedbyte, aesalg.Key, aesalg.IV);
+
+            
+            using (Stream file = File.OpenWrite(@"C:\\Users\\Pizza\\source\\repos\\ConsoleApp3\\ConsoleApp3\\testresult.txt"))
+            {
+                file.Write(outputbyte, 0, outputbyte.Length);
+            }
+
+
+                Console.WriteLine("Program dead everyone clap");
+        }
+        
+        static byte[] EncryptFileToBytes_Aes(byte[] plainText, byte[] Key, byte[] IV)                                                                  //this is from c# documentation at https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-5.0
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
                 {
-                    filetest[count] = b;
-                    count++;
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (BinaryWriter swEncrypt = new BinaryWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+        static byte[] DecryptFileFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)                                                                                 //this is from c# documentation at https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-5.0
+        {
+            {
+                // Check arguments.
+                if (cipherText == null || cipherText.Length <= 0)
+                    throw new ArgumentNullException("cipherText");
+                if (Key == null || Key.Length <= 0)
+                    throw new ArgumentNullException("Key");
+                if (IV == null || IV.Length <= 0)
+                    throw new ArgumentNullException("IV");
+
+                // Declare the string used to hold
+                // the decrypted text.
+                byte[] plaintext = null;
+                
+                // Create an Aes object
+                // with the specified key and IV.
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = Key;
+                    aesAlg.IV = IV;
+
+                    // Create a decryptor to perform the stream transform.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for decryption.
+                    using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (BinaryReader srDecrypt = new BinaryReader(csDecrypt))
+                            {
+                                // using (BinaryReader srDecrypt = new BinaryReader(csDecrypt))
+                                //{
+
+                                // Read the decrypted bytes from the decrypting stream
+                                // and place them in a string.
+
+                                srDecrypt.Read(cipherText);
+                            }
+                            plaintext = msDecrypt.ToArray();
+                        }
+                    }
                 }
 
-
+                return plaintext;
             }
-            
-
-            AeadAlgorithm x= AeadAlgorithm.Aes256Gcm;
-            
-            Span<int> num = new int[1];
-            Sha256 alg= new Sha256();    // dont use sha im stupid
-            Key a= Key.Create(alg);
-            Nonce n = new Nonce();
-            if(!filetest.IsEmpty)
-            x.Encrypt(a, n,null, filetest);
-            Console.WriteLine("Program dead everyone clap");
+        }
+        
         }
     }
-}
+
