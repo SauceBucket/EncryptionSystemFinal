758965cef62efabfbe4c9529bab8e0f64a8edfc6
rcs1014 <55924232+rcs1014@users.noreply.github.com>
2021-03-15 20:56:51 -0400

    Add files via upload

---
+++ EncryptionSystem-master/EncryptionSystem/EncryptionSystemForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace EncryptionSystem
+{
+    partial class EncryptionSystemForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.receiveBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.FileSelectBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(123, 274);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(109, 47);
+            this.encryptBtn.TabIndex = 4;
+            this.encryptBtn.Text = "Encrypt File";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(123, 358);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(109, 51);
+            this.decryptBtn.TabIndex = 5;
+            this.decryptBtn.Text = "Decrypt File";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(384, 408);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(221, 73);
+            this.exitBtn.TabIndex = 7;
+            this.exitBtn.Text = "Exit Application";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(755, 358);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(125, 51);
+            this.sendBtn.TabIndex = 9;
+            this.sendBtn.Text = "Send File";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // receiveBtn
+            // 
+            this.receiveBtn.Location = new System.Drawing.Point(755, 274);
+            this.receiveBtn.Name = "receiveBtn";
+            this.receiveBtn.Size = new System.Drawing.Size(125, 47);
+            this.receiveBtn.TabIndex = 10;
+            this.receiveBtn.Text = "Receive File";
+            this.receiveBtn.UseVisualStyleBackColor = true;
+            this.receiveBtn.Click += new System.EventHandler(this.receiveBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(384, 178);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 64);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // FileSelectBtn
+            // 
+            this.FileSelectBtn.Location = new System.Drawing.Point(384, 118);
+            this.FileSelectBtn.Name = "FileSelectBtn";
+            this.FileSelectBtn.Size = new System.Drawing.Size(221, 54);
+            this.FileSelectBtn.TabIndex = 12;
+            this.FileSelectBtn.Text = "Select File";
+            this.FileSelectBtn.UseVisualStyleBackColor = true;
+            this.FileSelectBtn.Click += new System.EventHandler(this.FileSelectBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Osu! File";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // EncryptionSystemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1001, 574);
+            this.ControlBox = false;
+            this.Controls.Add(this.FileSelectBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.receiveBtn);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Name = "EncryptionSystemForm";
+            this.Text = "Encryption System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Button receiveBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button FileSelectBtn;
+    }
+}
+
